f1e018b0a1623471b0720eec3a11da15e344d9be
Marko Cupic <m.cupic@gmx.ch>
2020-07-16 18:40:56 +0200

    Update SampleModule.php

--- src/Samples/sample-repository/src/Controller/FrontendModule/SampleModule.php
+++ src/Samples/sample-repository/src/Controller/FrontendModule/SampleModule.php
@@ -7,10 +7,19 @@ declare(strict_types=1);
 namespace #toplevelnamespace#\#sublevelnamespace#\Controller\FrontendModule;
 
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Contao\CoreBundle\Routing\ScopeMatcher;
+use Contao\Date;
+use Contao\FrontendUser;
 use Contao\ModuleModel;
+use Contao\PageModel;
 use Contao\Template;
+use Doctrine\DBAL\Connection;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
  * Class #frontendmoduleclassname#
@@ -18,10 +27,99 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class #frontendmoduleclassname# extends AbstractFrontendModuleController
 {
+    /**
+     * @var PageModel
+     */
+    protected $page;
+
+    /**
+     * #frontendmoduleclassname# constructor.
+     * @param SessionInterface $session
+     */
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * This method extends the parent __invoke method,
+     * its usage is usually not necessary
+     * @param Request $request
+     * @param ModuleModel $model
+     * @param string $section
+     * @param array|null $classes
+     * @param PageModel|null $page
+     * @return Response
+     */
+    public function __invoke(Request $request, ModuleModel $model, string $section, array $classes = null, PageModel $page = null): Response
+    {
+        // Get the page model
+        $this->page = $page;
+
+        if ($this->page instanceof PageModel && $this->get('contao.routing.scope_matcher')->isFrontendRequest($request))
+        {
+            // If TL_MODE === 'FE'
+            $this->page->loadDetails();
+        }
+
+        return parent::__invoke($request, $model, $section, $classes);
+    }
+
+    /**
+     * Lazyload some services
+     * @return array
+     */
+    public static function getSubscribedServices(): array
+    {
+        $services = parent::getSubscribedServices();
+
+        $services['contao.framework'] = ContaoFramework::class;
+        $services['database_connection'] = Connection::class;
+        $services['contao.routing.scope_matcher'] = ScopeMatcher::class;
+        $services['security.helper'] = Security::class;
+        $services['translator'] = TranslatorInterface::class;
+
+        return $services;
+    }
+
+    /**
+     * @param Template $template
+     * @param ModuleModel $model
+     * @param Request $request
+     * @return null|Response
+     */
     protected function getResponse(Template $template, ModuleModel $model, Request $request): ?Response
     {
-        $template->helloTitle = 'Welcome to the "Hello World Module"';
-        $template->helloText = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+        $userFirstname = 'DUDE';
+        $user = $this->get('security.helper')->getUser();
+        if ($user instanceof FrontendUser)
+        {
+            $userFirstname = $user->firstname;
+        }
+
+        /** @var Date $dateAdapter */
+        $dateAdapter = $this->get('contao.framework')->getAdapter(Date::class);
+        $intWeekday = $dateAdapter->parse('w');
+        $translator = $this->get('translator');
+        $strWeekday = $translator->trans('DAYS.' . $intWeekday, [], 'contao_default');
+
+        $arrGuests = [];
+        $stmt = $this->get('database_connection')
+            ->executeQuery(
+                'SELECT * FROM tl_member WHERE gender=? ORDER BY lastname',
+                ['female']
+            );
+        while (false !== ($objMember = $stmt->fetch(\PDO::FETCH_OBJ)))
+        {
+            $arrGuests[] = $objMember->firstname;
+        }
+
+        $template->helloTitle = sprintf(
+            'Hi %s, and welcome to the "Hello World Module". Today is %s.',
+            $userFirstname, $strWeekday
+        );
+
+        $template->helloText = 'Our guests today are: ' . implode(', ', $arrGuests);
 
         return $template->getResponse();
     }
