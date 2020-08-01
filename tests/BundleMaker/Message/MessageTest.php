914c54ef54a52198005d6ea7fe2be62589476252
Marko Cupic <m.cupic@gmx.ch>
2020-08-01 18:40:35 +0200

    Create MessageTest.php

---
+++ tests/BundleMaker/Message/MessageTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @copyright  Marko Cupic 2020 <m.cupic@gmx.ch>
+ * @author     Marko Cupic
+ * @package    RSZ Mein Steckbrief
+ * @license    MIT
+ * @see        https://github.com/markocupic/rsz-steckbrief-bundle
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Markocupic\ContaoBundleMakerBundle\Tests\BundleMaker\Message;
+
+use Contao\System;
+use Contao\TestCase\ContaoTestCase;
+use Markocupic\ContaoBundleCreatorBundle\BundleMaker\Message\Message;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
+
+/**
+ * Class MessageTest
+ *
+ * @package Markocupic\ContaoBundleMakerBundle\Tests\BundleMaker\Message
+ */
+class MessageTest extends ContaoTestCase
+{
+
+    /** @var string */
+    protected $tmpTargetFile;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        System::setContainer($this->getContainerWithContaoConfiguration());
+
+        $session = new Session(new MockArraySessionStorage());
+        $this->message = new Message($session);
+    }
+
+    public function testInstantiation(): void
+    {
+        $this->assertInstanceOf(Message::class, $this->message);
+    }
+
+    public function testAdd(): void
+    {
+        $this->message->addInfo('Info text 1.');
+        $this->message->addInfo('Info text 2.');
+
+        $this->message->addError('Error text 1.');
+        $this->message->addError('Error text 2.');
+
+        $this->assertSame('Info text 1.', $this->message->getInfo()[0]);
+        $this->assertSame('Error text 2.', $this->message->getError()[1]);
+    }
+}
