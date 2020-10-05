9847ba0a5362afbb47454ad255642ca678bdd013
Marko Cupic <m.cupic@gmx.ch>
2020-10-05 20:07:51 +0200

    added ecs batch files to skeleton

---
+++ src/Resources/skeleton/.ecs/batch/fix/all.tpl.bat
@@ -0,0 +1,18 @@
+:: Run easy-coding-standard (ecs) via this batch file inside your IDE e.g. PhpStorm (Windows only)
+:: Install inside PhpStorm the  "Batch Script Support" plugin
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+:: src
+vendor\bin\ecs check vendor/<?= $this->vendorname ?>/<?= $this->repositoryname ?>/src --fix --config vendor/<?= $this->vendorname ?>/<?= $this->repositoryname ?>/.ecs/config/default.yaml
+:: tests
+vendor\bin\ecs check vendor/<?= $this->vendorname ?>/<?= $this->repositoryname ?>/tests --fix --config vendor/<?= $this->vendorname ?>/<?= $this->repositoryname ?>/.ecs/config/default.yaml
+:: legacy
+vendor\bin\ecs check vendor/<?= $this->vendorname ?>/<?= $this->repositoryname ?>/src/Resources/contao --fix --config vendor/<?= $this->vendorname ?>/<?= $this->repositoryname ?>/.ecs/config/legacy.yaml
+:: templates
+vendor\bin\ecs check vendor/<?= $this->vendorname ?>/<?= $this->repositoryname ?>/src/Resources/contao/templates --fix --config vendor/<?= $this->vendorname ?>/<?= $this->repositoryname ?>/.ecs/config/template.yaml
+::
+cd vendor/<?= $this->vendorname ?>/<?= $this->repositoryname ?>/.ecs./batch/fix
---
+++ src/Resources/skeleton/.ecs/batch/fix/template.tpl.bat
@@ -0,0 +1,12 @@
+:: Run easy-coding-standard (ecs) via this batch file inside your IDE e.g. PhpStorm (Windows only)
+:: Install inside PhpStorm the  "Batch Script Support" plugin
+cd..
+cd..
+cd..
+cd..
+cd..
+cd..
+:: templates
+vendor\bin\ecs check vendor/<?= $this->vendorname ?>/<?= $this->repositoryname ?>/src/Resources/contao/templates --fix --config vendor/<?= $this->vendorname ?>/<?= $this->repositoryname ?>/.ecs/config/template.yaml
+::
+cd vendor/<?= $this->vendorname ?>/<?= $this->repositoryname ?>/.ecs./batch/fix
