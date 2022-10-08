6172eaa6a2b4cb01d7b0164ad346b42e2196b3b7
Marko Cupic <m.cupic@gmx.ch>
2022-10-08 16:15:29 +0200

    ecs skip dca and skeleton

--- .ecs/config/default.php
+++ .ecs/config/default.php
@@ -7,6 +7,12 @@ use Symplify\EasyCodingStandard\Config\ECSConfig;
 return static function (ECSConfig $ECSConfig): void {
     // Contao
     $ECSConfig->import(__DIR__ . '../../../../../contao/easy-coding-standard/config/contao.php');
+
     // Custom
     $ECSConfig->import(__DIR__.'/set/header_comment_fixer.php');
+
+    // Skip
+    $ECSConfig->import(__DIR__.'/set/skip_dca.php');
+    $ECSConfig->import(__DIR__.'/set/skip_skeleton.php');
+
 };
